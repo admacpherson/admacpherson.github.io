f2ce8ef08ae80f821e4e1ae8b9c81818ba787a26
admacpherson <admacpherson@users.noreply.github.com>
2025-08-16 23:34:06 +0000

    Deploy site via GitHub Actions bec04bada808e8b66eed2555bdf49b6ea7310c9c

---
+++ word-games/letter-boxed/boards.js
@@ -0,0 +1,44 @@
+export const gameBoards = [
+    {
+        top: ['W', 'M', 'Y'],
+        right: ['T', 'O', 'I'],
+        bottom: ['H', 'N', 'D'],
+        left: ['S', 'L', 'R']
+    },
+    {
+        top: ['E', 'M', 'C'],
+        right: ['O', 'T', 'J'],
+        bottom: ['N', 'X', 'I'],
+        left: ['V', 'A', 'U']
+    },
+    {
+        top: ['P', 'A', 'L'],
+        right: ['E', 'B', 'O'],
+        bottom: ['T', 'N', 'R'],
+        left: ['S', 'C', 'D']
+    },
+    {
+        top: ['G', 'H', 'T'],
+        right: ['U', 'E', 'S'],
+        bottom: ['B', 'I', 'N'],
+        left: ['C', 'L', 'A']
+    },
+    {
+        top: ['F', 'R', 'O'],
+        right: ['Z', 'A', 'L'],
+        bottom: ['M', 'D', 'T'],
+        left: ['N', 'U', 'E']
+    },
+    {
+        top: ['Q', 'C', 'I'],
+        right: ['U', 'K', 'B'],
+        bottom: ['R', 'O', 'N'],
+        left: ['T', 'E', 'S']
+    },
+    {
+        top: ['D', 'R', 'Y'],
+        right: ['F', 'I', 'T'],
+        bottom: ['C', 'O', 'L'],
+        left: ['S', 'A', 'E']
+    }
+];
